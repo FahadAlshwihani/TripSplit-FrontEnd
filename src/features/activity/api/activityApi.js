4176ac49842fcaad02ac5811004080c003b206b5
FahadAlshwihani <4is23ib@gmail.com>
2026-08-20 13:55:13 +0300

    refactor(trips): add focused overview settings balances and activity routes

--- src/features/activity/api/activityApi.js
+++ src/features/activity/api/activityApi.js
@@ -1,2 +1,3 @@
 import { apiClient, responseData } from '../../../api/client'; import { tripRequest } from '../../../api/credentials';
 export const getActivity = (id, config) => responseData(apiClient.get(`/trips/${id}/activity/`, tripRequest(id, config)));
+export const getActivityPage = (url,id,config) => responseData(apiClient.get(url,tripRequest(id,config)));
